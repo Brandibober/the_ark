aa3a1d608df990efbe1802ad3c063f8895d8c319
TheRedHotHabanero <lirisman.ks@phystech.edu>
2021-05-04 20:48:25 +0300

    better file splitting (menu.h, menu.cpp)

--- menu.h
+++ menu.h
@@ -43,80 +43,25 @@ protected:
     int menuNum = 0;
 public:
 
-    void create_menu(const string& menu_)
-    {
-        menu.loadFromFile(menu_);
-        menu_texture.loadFromImage(menu);
-        menu_sprite.setTexture(menu_texture);
-        menu_sprite.setPosition(MENU_POSITION_X, MENU_POSITION_Y);
-    }
+    //---------------------------------------creating sprites for menu:
+    void create_menu(const string& menu_);
+    void create_start(const string& start_);
+    void create_exit(const string& exit_);
+    //------------------------------------------------------------------
 
-    void create_start(const string& start_)
-    {
-        start.loadFromFile(start_);
-        start_texture.loadFromImage(start);
-        start_sprite.setTexture(start_texture);
-        start_sprite.setPosition(START_BUTTON_POS_X, START_BUTTON_POS_Y);
-    }
-    void create_exit(const string& exit_)
-    {
-        exit.loadFromFile(exit_);
-        exit_texture.loadFromImage(exit);
-        exit_sprite.setTexture(exit_texture);
-        exit_sprite.setPosition(EXIT_BUTTON_POS_X, EXIT_BUTTON_POS_Y);
-    }
 
-    void set_flight()
-    { IS_FLIGHT_AVAILABLE = YES; }
+    //-----------------------------changing and getting flight condition:
+    void set_flight();                                              //makes the flight available
+    bool get_flight();                                              //gets flight condition
+    //------------------------------------------------------------------
 
-    bool get_flight()
-    { return IS_FLIGHT_AVAILABLE; }
 
-    void start_pressed(RenderWindow& window)
-    {
-        set_flight();
-        window.close();
-    }
-
-    void welcome_page(string menu_, string exit_, string start_)
-    {
-        RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "THE ARK");
-        create_menu(menu_);
-        create_exit(exit_);
-        create_start(start_);
-        window.clear();
-        window.draw(menu_sprite);
-        window.draw(exit_sprite);
-        window.draw(start_sprite);
-        window.display();
-        processing_menu(window);
-    }
-
-    void exit_pressed(RenderWindow& window)
-    { window.close(); }
-
-    void processing_menu(RenderWindow& window)
-    {
-        while (window.isOpen())
-        {
-            Event event;
-
-            //while (window.pollEvent(event))
-            if (event.type == Event::Closed || Keyboard::isKeyPressed(Keyboard::Escape))
-                exit_pressed(window);
-
-            if (IntRect(START_BUTTON_POS_X, START_BUTTON_POS_Y, START_BUTTON_WIDTH, START_BUTTON_HEIGHT).contains(Mouse::getPosition(window))) { menuNum = START; }
-            if (IntRect(EXIT_BUTTON_POS_X, EXIT_BUTTON_POS_Y, EXIT_BUTTON_WIDTH, EXIT_BUTTON_HEIGHT).contains(Mouse::getPosition(window))) { menuNum = EXIT; }
-
-            if (Mouse::isButtonPressed(Mouse::Left))
-            {
-                if (menuNum == START)
-                    start_pressed(window);
-                else if (menuNum == EXIT)
-                    exit_pressed(window);
-            }
-        }
-    }
+    //------------------------------------------------processing bottons:
+    void start_pressed(RenderWindow& window);                       //start booton
+    void welcome_page(string menu_, string exit_, string start_);   //menu
+    void exit_pressed(RenderWindow& window);                        //exit botton
+    void processing_menu(RenderWindow& window);                     //processing the previos
+    //------------------------------------------------------------------
 };
 
 #define THE_ARK_MENU_H
