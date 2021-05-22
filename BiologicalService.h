edcebf685c2a695b9878d435d7fc31fa75effb5e
DvornikES <69252174+DvornikES@users.noreply.github.com>
2021-05-22 20:42:57 +0300

    Kostya's 4th commit

--- BiologicalService.h
+++ BiologicalService.h
@@ -18,29 +18,27 @@ public:
     double getState() override;                             // каждая служба должна уметь вернуть свое состояние в процентах, посчитав его в своих терминах
     void setState(double s) override;                       // функция для инициализации, каждая служба должна уметь получить состояние в процентах и пересчитать  его в своих терминах
 };
-
 class BioRes
 {
 private:
 int k,mass,junk,worker_0;
 double hp,KPD;
 public:
-BioRes(unsigned int start_mass,int consume_for1);
-RecycleRes(int worker,int Energy,int people);
-profit(int worker);
-BioRes::GetResource();
+BioRes(unsigned int start_mass,double KPD_e,int worker_optimum,int max_profit,double health_of_system, int consume_for1);
+void RecycleRes(int worker,int Energy,int people);
+int profit(int worker);
+int GetResource();
 
-BioRes::GetJunk();
+double GetJunk();
 
-BioRes::Gethp();
+double Gethp();
 
-BioRes::Sethp(double health_of_system);
+void Sethp(double health_of_system);
 
-BioRes::GetPeople(int worker):
+int GetPeople(int worker);
 
 
-}
-
+};
 struct coord;
 
 int GetTypeId (int x);
